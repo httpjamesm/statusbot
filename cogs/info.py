58e186c787fad0e911299b37b2217449b76d8cc8
james <james@james.james>
2021-02-27 13:40:18 -0500

    add ping command

--- cogs/info.py
+++ cogs/info.py
@@ -8,6 +8,7 @@ class info(commands.Cog):
         # Re-define the bot object into the class.
 
     @commands.command(aliases=["about","info"])
+    @commands.cooldown(1,10,commands.BucketType.user)
     async def help(self,ctx):
         embed=discord.Embed(title="StatusBot Help", description="StatusBot is a Discord bot that can create real-time status pages within Discord communities for any host that supports IPv4 connectivity.\n\nYou can invoke this menu at anytime using `" + settings.configdata["prefix"] + "help`.", color=0xff0000)
         embed.add_field(name="Instance", value="**Name**: " + settings.configdata["instance_name"] + "\nMonitor Limit: " + str(settings.configdata["host_limit"]), inline=False)
@@ -20,6 +21,11 @@ class info(commands.Cog):
             await ctx.send(f"{ctx.author.mention} :warning: I couldn't send you a DM, do you have DMs enabled?")
             return
         await ctx.send(f"{ctx.author.mention} :white_check_mark: I've sent you a DM!")
+
+    @commands.command(aliases=["latency"])
+    @commands.cooldown(1,10,commands.BucketType.guild)
+    async def ping(self,ctx):
+        await ctx.send(":ping_pong: Pong! API Latency: " + str(round(self.bot.latency*100,2)) + " ms.")
 
 def setup(bot):
     bot.add_cog(info(bot))
